73bd87ac384d0aef8465fc63e02e5a61f935f578
elio3 <elio@pixel38.com>
2024-10-10 16:52:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Menu from "./screens/Menu/Menu";
+import Dashboard from "./screens/Dashboard";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
